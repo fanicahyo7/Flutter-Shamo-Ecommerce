adb2827e146cc5b9a0f2400d50abadd9215cc59c
fanicahyo7 <fanidc7@gmail.com>
2021-09-10 19:05:48 +0700

    theme

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xff6C5ECF);
+Color kSecondaryColor = Color(0xff38ABBE);
+Color kAlertColor = Color(0xffED6363);
+Color kPriceColor = Color(0xff2C96F1);
+Color kBgColor1 = Color(0xff1F1D2B);
+Color kBgColor2 = Color(0xff2B2937);
+Color kBgColor3 = Color(0xff242231);
+Color kPrimaryTextColor = Color(0xffF1F0F2);
+Color kSecondaryTextColor = Color(0xff999999);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: kPrimaryTextColor);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: kSecondaryTextColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
